0a54f3b94b49954621250a4adfd3d72aadafc66a
darshankumar052004-cell <darshan.kumar052004@gmail.com>
2026-05-15 01:16:59 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.4" apply false
+}
